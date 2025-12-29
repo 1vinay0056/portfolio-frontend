6853b6fd11966010dad4284d756b6caf8a98d9be
1vinay0056 <vinaykumarmth78@gmail.com>
2025-12-30 02:09:29 +0530

    fix: footer routing links

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+import { FaTwitter, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -10,19 +10,32 @@ export default function Footer() {
 
       {/* Navigation Links */}
       <ul className="footer-links">
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/skills">Skills</Link></li>
         <li><Link to="/experience">Experience</Link></li>
         <li><Link to="/education">Education</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
 
       {/* Social Icons */}
       <div className="footer-social">
-        <a href="https://x.com/VinayYa00559523"><FaTwitter /></a>
-        <a href="https://www.linkedin.com/in/vinay-kumar-68a3492a5" target="_blank"><FaLinkedin /></a>
-        <a href="https://www.instagram.com/raoo___saab/?igsh=ejllNDRlZjhrN3lw#"><FaInstagram /></a>
-        <a href="https://github.com/1vinay0056" target="_blank"><FaGithub /></a>
+        <a href="https://x.com/VinayYa00559523" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+
+        <a href="https://www.linkedin.com/in/vinay-kumar-68a3492a5" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+
+        <a href="https://www.instagram.com/raoo___saab" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+
+        <a href="https://github.com/1vinay0056" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
       </div>
 
       {/* Copyright */}
